01f0c7296cecb2724a1322d9b12fd98415a87c07
Peter Höffling <p.hoeffling@bit-it.com>
2015-02-20 06:49:45 +0100

    Update test to use Hamcrest matchers.

--- src/test/java/org/springframework/social/gitlab/api/profile/impl/GitlabProfileTemplateTest.java
+++ src/test/java/org/springframework/social/gitlab/api/profile/impl/GitlabProfileTemplateTest.java
@@ -1,6 +1,7 @@
 package org.springframework.social.gitlab.api.profile.impl;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.social.gitlab.api.AbstractGitlabApiTest;
@@ -25,26 +26,27 @@ public class GitlabProfileTemplateTest extends AbstractGitlabApiTest {
                 .andRespond(withJsonResourceSuccess("gitlab-profile"));
         
         GitlabProfile userProfile = gitlab.profileOperations().getProfile();
-        assertEquals(1, userProfile.getId());
-        assertEquals("john_smith", userProfile.getUsername());
-        assertEquals("john@example.com", userProfile.getEmail());
-        assertEquals("John Smith", userProfile.getName());
-        assertEquals("dd34asd13as", userProfile.getPrivateToken());
-        assertEquals("active", userProfile.getState());
-        verifyUtcDate(userProfile.getCreatedAt(), 2012, 5, 23, 8, 0, 58);
-        assertEquals("bio", userProfile.getBio());
-        assertEquals("skype", userProfile.getSkype());
-        assertEquals("linkedin", userProfile.getLinkedin());
-        assertEquals("twitter", userProfile.getTwitter());
-        assertEquals("website_url", userProfile.getWebsiteUrl());
-        assertEquals("http://localhost:3000/uploads/user/avatar/1/cd8.jpeg", userProfile.getAvatarUrl());
-        assertEquals(1, userProfile.getThemeId());
-        assertEquals(2, userProfile.getColorSchemeId());
-        assertEquals(false, userProfile.isAdmin());
-        assertEquals(true, userProfile.canCreateGroup());
-        assertEquals(true, userProfile.canCreateProject());
-        assertEquals(100, userProfile.getProjectsLimit());
+        assertThat(userProfile.getId(), is(1L));
+        assertThat(userProfile.getUsername(), is("john_smith"));
+        assertThat(userProfile.getEmail(), is("john@example.com"));
+        assertThat(userProfile.getName(), is("John Smith"));
+        assertThat(userProfile.getPrivateToken(), is("dd34asd13as"));
+        assertThat(userProfile.getBio(), is("bio"));
+        assertThat(userProfile.getSkype(), is("skype"));
+        assertThat(userProfile.getLinkedin(), is("linkedin"));
+        assertThat(userProfile.getTwitter(), is("twitter"));
+        assertThat(userProfile.getWebsiteUrl(), is("website_url"));
+        assertThat(userProfile.getAvatarUrl(), is("http://localhost:3000/uploads/user/avatar/1/cd8.jpeg"));
         
+        assertThat(userProfile.getThemeId(), is(1L));
+        assertThat(userProfile.getColorSchemeId(), is(2L));
+        assertThat(userProfile.getProjectsLimit(), is(100L));
+        
+        assertThat(userProfile.isAdmin(), is(false));
+        assertThat(userProfile.canCreateGroup(), is(true));
+        assertThat(userProfile.canCreateProject(), is(true));
+
+        verifyUtcDate(userProfile.getCreatedAt(), 2012, 5, 23, 8, 0, 58);
     }
     
 }
